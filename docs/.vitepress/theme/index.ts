34fafffe4c7500ec9bdaa320fd64cafbbc09846e
rainlam163 <490106135@qq.com>
2024-08-27 19:41:15 +0800

    自定义指令

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,6 @@
+import type { Theme } from 'vitepress'
 import BlogTheme from '@sugarat/theme'
+import vClickable from 'v-clickable'
 
 // 自定义样式重载
 import './style.scss'
@@ -6,4 +8,14 @@ import './style.scss'
 // 自定义主题色
 // import './user-theme.css'
 
-export default BlogTheme
+const theme: Theme = {
+    ...BlogTheme,
+    enhanceApp(ctx) {
+        if (BlogTheme.enhanceApp) {
+            BlogTheme.enhanceApp(ctx)
+        }
+        ctx.app.use(vClickable)
+    }
+}
+
+export default theme
